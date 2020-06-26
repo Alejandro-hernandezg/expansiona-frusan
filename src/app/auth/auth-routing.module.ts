901a72cc540c660ffa6b13accdc7945b86dc9a80
ricardojbj <ric.ucvmicrop@gmail.com>
2020-06-26 19:49:28 -0400

    feat: add routes behavior at startup

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -13,6 +13,11 @@ const routes: Routes = [
         path: 'login',
         component: LoginComponent, // <---
       },
+      {
+        path: '',
+        redirectTo: 'login',
+        pathMatch: 'full',
+      }
     ],
   },
 ];
